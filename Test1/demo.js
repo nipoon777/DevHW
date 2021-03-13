27986dd4ea56a27d3dbd93548d259a4c51c35c7a
nipoon777 <nipoon.donta7@gmail.com>
2021-03-13 10:20:37 +0530

    Test Tryout

---
+++ Test1/demo.js
@@ -0,0 +1,38 @@
+// const num = 5;
+// console.log(num + 5);
+// let a = 6;
+// a = a + num;
+// console.log(num - a);
+
+// let a = 2;
+// {
+//  let a = 3;
+//  {
+//  let a = 4;
+//  {
+//  let a = 5;
+//  console.log(a);
+//  }
+//  console.log(a);
+//  }
+//  console.log(a);
+// }
+// console.log(a);
+
+// let input = [
+//     { name: "Roorkee", rainfall: [5, 6, 5, 5, 4, 7, 8] },
+//     { name: "Pauri", rainfall: [3, 3, 3, 1, 2, 2, 2] },
+//    ];
+
+// for(let i = 0 ; i < input.length ; i++ ){
+//     let sum = 0;
+//     for( let j = 0 ; j < input[i].rainfall.length ; j++ ){
+//         sum += input[i].rainfall[j];
+//     }
+
+//     input[i]["rainfall"] = (sum * 1.0 % input[i].rainfall.length);
+
+// }
+// console.log(input);
+let a;
+console.log(a);
